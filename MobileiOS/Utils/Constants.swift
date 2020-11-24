e241a1ad46e3dd28d0ea6c6b92db01f9cab46597
George Mihoc <george.mihoc@gmail.com>
2020-11-24 12:34:32 +0200

    Localhost changed to current ip adress to work on iPhone

--- MobileiOS/Utils/Constants.swift
+++ MobileiOS/Utils/Constants.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Constants{
     
     static let nodeApi = "http://\(baseURL)/api/item"
-    static let baseURL = "127.0.0.1:3000"
+    static let baseURL = "192.168.0.80:3000"
     static let authApi = "http://\(baseURL)/api/auth/login"
     static let downloadLink = "https://raw.githubusercontent.com/georgemihoc/MobileiOS/main/tweets.json"
 }
